628232f4ef9f333e46d662416b8280468b469552
David Antillies <93983691+dongyan3721@users.noreply.github.com>
2024-03-06 21:28:04 +0800

    新增依赖

---
+++ setup-dependicies.bat
@@ -0,0 +1,13 @@
+pip install fastapi==0.109.0
+pip install fastapi-pagination==0.12.14
+pip install httpx==0.27.0
+pip install PyJWT==2.3.0
+pip install PyYAML==6.0
+pip install aerich==0.7.2
+pip install aiomysql==0.2.0
+pip install aiosqlite==0.17.0
+pip install asyncmy==0.2.9
+pip install pymongo==4.6.1
+pip install rsa==4.9
+pip install tortoise-orm==0.20.0
+pip install uvicorn==0.25.0
